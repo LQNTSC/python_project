6b539b13d88b36b2f706678e1f4a36a381a3a7af
witucas <wit_1104010217@163.com>
2017-08-11 12:40:43 +0800

    add the method of log

---
+++ c2.py
@@ -0,0 +1,13 @@
+#code = utf-8
+import flask
+
+from flask import Flask
+
+app = Flask(__name__)
+@app.route('/')
+
+def index():
+    return 'hello'
+
+if __name__ == '__main__':
+    app.run(debug = True)
